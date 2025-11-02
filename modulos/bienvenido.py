6f0dc65da69995dae4b5df940d44989e82149c54
00126222-dev <00126222@uca.edu.sv>
2025-11-01 18:53:25 -0600

    Create bienvenido.py

---
+++ modulos/bienvenido.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def mostrar_bienvenido():
+    # Aquí se muestra "Hola Mundo"
+    st.title("Bienvenido")
+    st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
