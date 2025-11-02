f97719f2c75f7d5dbc03fa3d1dbbb9ac1576fad3
00126222-dev <00126222@uca.edu.sv>
2025-11-01 20:40:53 -0600

    Delete modulos directory

--- modulos/bienvenido.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def mostrar_bienvenido():
-    # Aquí se muestra "Hola Mundo"
-    st.title("Bienvenido")
-    st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
